26b5b876f107d633d5bf632295344434f3e46f02
文 <45875318+asus0420@users.noreply.github.com>
2020-01-26 23:00:14 +0800

    Add files via upload

--- Sample_Tcp_Client/client.cpp
+++ Sample_Tcp_Client/client.cpp
@@ -5,10 +5,61 @@
 #include <iostream>
 using namespace std;
 
-struct  DataPackage
+//命令枚举
+enum Operate
 {
-	int age;
-	char name[32];
+	Server_Login,
+	Server_Login_Result,
+	Server_Logout,
+	Server_Logout_Result,
+	Server_Error,
+};
+
+struct DataHeader
+{
+	int headerLen;//包头：数据长度
+	int operate; //操作
+};
+//登录成功
+struct Login :public DataHeader
+{
+	Login()
+	{
+		headerLen = sizeof(Login);
+		operate = Server_Login;
+	}
+	char userName[32];
+	char passWord[32];
+};
+struct LoginResult :public DataHeader
+{
+	LoginResult()
+	{
+		headerLen = sizeof(LoginResult);
+		operate = Server_Login_Result;
+		result = 0;
+	}
+	int result;
+};
+//登录失败
+struct LogOut :public DataHeader
+{
+	LogOut()
+	{
+		headerLen = sizeof(LogOut);
+		operate = Server_Logout;
+	}
+	char userName[32];
+};
+struct LogOutResult :public DataHeader
+{
+	LogOutResult()
+	{
+		headerLen = sizeof(LogOutResult);
+		operate = Server_Logout_Result;
+		result = 0;
+	}
+	int result;
 };
 
 
@@ -50,22 +101,34 @@ int main()
 			printf("客户端执行exit,客户端退出\n");
 			break; 
 		}
-		else
+		else if ( 0 == strcmp(_cmdMsg,"login"))
 		{
-			send(_sock, _cmdMsg, strlen(_cmdMsg) + 1, 0);
-		}
+			Login  _lgin;
+			strcpy_s(_lgin.userName, "asus0420");
+			strcpy_s(_lgin.passWord, "mw970420");
+			//发送数据
+			send(_sock, (const char *)&_lgin, sizeof(_lgin), 0);
+			//接受服务端返回的数据
+			LoginResult _lgRes = {};
+			recv(_sock, (char *)&_lgRes, sizeof(_lgRes), 0);
+			printf("接收到的数据:%d \n", _lgRes.result);
 
-		//3.recv
-		char _tips[128] = {};
-		int rev = recv(_sock, _tips, 128, 0);
-		if (rev > 0)
+		}
+		else if (0 == strcmp(_cmdMsg,"logout"))
 		{
-			DataPackage *dp = (DataPackage *)_tips;
-			printf("接收到数据：年龄 = %d , 姓名 = %s \n", dp->age,dp->name);
+			LogOut _lgout;
+			strcpy_s(_lgout.userName, "asus0420");
+			//发送数据
+			send(_sock, (const char *)&_lgout, sizeof(_lgout), 0);
+
+			//接受服务器的返回的数据
+			LogOutResult _lgoutRes = {};
+			recv(_sock, (char *)&_lgoutRes, sizeof(_lgoutRes), 0);
+			printf("接收到的数据:%d  \n", _lgoutRes.result);
 		}
 		else
 		{
-			printf("未接收到数据\n");
+			printf("该命令不支持，请重新输入...\n");
 		}
 	}
 
